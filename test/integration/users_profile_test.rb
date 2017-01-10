187cbefb6542d67aca79d5c8da5df4da7762be27
Len Lee <lenlee454@gmail.com>
2017-01-10 14:09:29 -0800

    Add user following

--- test/integration/users_profile_test.rb
+++ test/integration/users_profile_test.rb
@@ -18,5 +18,8 @@ class UsersProfileTest < ActionDispatch::IntegrationTest
     @user.microposts.paginate(page: 1).each do |micropost|
       assert_match micropost.content, response.body
     end
+    #Stats should show number of following and followers
+    assert_match @user.followers.count.to_s, response.body
+    assert_match @user.followers.count.to_s, response.body
   end
 end
